d0b2bac5e5aa788465fc0899a3e7684821687cbf
sahil-mor <iconsahil7@gmail,com>
2019-06-30 15:01:25 +0530

    adding files

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.render("home.ejs")
+})
+
+app.post("/locationAPI",function(req,res){
+    city = req.body.city;
+    console.log("City - " + city)
+    url = "http://api.apixu.com/v1/forecast.json?key=714f3da0c9aa41a380d63653190606&q="
+    url += city;
+    request(url,function(error,response,body){
+        if(!error && response.statusCode == 200){
+            parsedCityDetails = JSON.parse(body);
+            res.render("WeatherData.ejs",{Data : parsedCityDetails})
+        }
+        else
+            res.send("error");
+    })
+})
+
+app.listen(1000,function(){
+    console.log("Weather app server is started")
+})
